8bcc3f0aa4495af0aa992a7a32ac1aa687a05eb1
Ganesh Kumar <gk48605@gmail.com>
2025-07-19 08:36:12 +0000

    admin jab admin dashboard me login h to usako ek welcome msg show ho wel

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -88,6 +88,10 @@ const data = [
 export default function AdminDashboard() {
   return (
     <div className="flex flex-col gap-4">
+      <div>
+        <h1 className="text-3xl font-bold tracking-tight">Welcome Admin!</h1>
+        <p className="text-muted-foreground">Welcome back, Admin. Here's an overview of your Payment gateway.</p>
+      </div>
       <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
