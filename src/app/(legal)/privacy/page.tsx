17dfe5b0536570fdfc800c40bfa2d578d2d69530
Ganesh Kumar <gk48605@gmail.com>
2025-08-07 10:46:25 +0000

    abhi text chota show ho raha ahi

--- src/app/(legal)/privacy/page.tsx
+++ src/app/(legal)/privacy/page.tsx
@@ -2,7 +2,7 @@
 export default function PrivacyPolicyPage() {
     return (
         <div>
-            <h1 className="text-center">Privacy Policy for UniversalPay</h1>
+            <h1 className="text-3xl font-bold text-center">Privacy Policy for UniversalPay</h1>
             <p><strong>Last updated:</strong> {new Date().toLocaleDateString()}</p>
             <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
             <p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy.</p>
